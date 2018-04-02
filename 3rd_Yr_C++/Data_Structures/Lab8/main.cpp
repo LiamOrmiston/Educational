f495f6dd9b6aece529160a4f172b5a04e6e86460
LiamOrmiston <ljormiston@ku.edu>
2018-04-02 15:44:38 -0500

    maxmin heap working fully

--- 3rd_Yr_C++/Data_Structures/Lab8/main.cpp
+++ 3rd_Yr_C++/Data_Structures/Lab8/main.cpp
@@ -5,9 +5,20 @@
  * Main file for executing program
  */
 #include "Execute.h"
+#include "ExecuteMax.h"
+#include <string.h>
 
 int main(int argc, char const *argv[]) {
-Execute exec;
-exec.run();
+  if(strcmp(argv[1],"minmax")==0){
+    Execute exec;
+    exec.run();
+  }
+  else if (strcmp(argv[1],"maxmin")==0) {
+    ExecuteMax exec;
+    exec.run();
+  }
+  else {
+    std::cout << "Please enter either 'minmax' or 'maxmin'" << '\n';
+  }
 return 0;
 };
